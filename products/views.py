a31f9e88342422172897a2e978fdc1c3541415e1
rafah <rafah.hawa@yahoo.fr>
2023-10-06 21:26:44 +0200

    add views , templates , urls , static part 1

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product , Brand , Review
 
+class ProductList(generic.ListView):
+    model = Product
+
+
+class ProductDetail(generic.DetailView):
+    model = Product
 # Create your views here.
