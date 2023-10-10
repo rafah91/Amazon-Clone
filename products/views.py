7c7213af04304cb893b485b7794438abf31b6aa1
rafah <rafah.hawa@yahoo.fr>
2023-10-10 11:47:44 +0200

    add reviews

--- products/views.py
+++ products/views.py
@@ -13,7 +13,13 @@ class ProductList(generic.ListView):
 
 class ProductDetail(generic.DetailView):
     model = Product
-# Create your views here.
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+    
+
 
 class BrandList(generic.ListView):
     model = Brand
@@ -26,7 +32,7 @@ class BrandDetail(generic.ListView):
         brand = Brand.objects.get(slug=self.kwargs['slug'])
         queryset = super().get_queryset().filter(brand=brand)
         return queryset
-    def get_context_data(self, **kwargs) :
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
         return context
