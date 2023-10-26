31977e5d45d1f5e7685c845f6f24cca8ea49b4b2
rafah <rafah.hawa@yahoo.fr>
2023-10-26 18:33:43 +0200

    show review count

--- products/serializers.py
+++ products/serializers.py
@@ -7,16 +7,24 @@ from .models import Product , Brand , Review , ProductImages
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
         
+    def get_review_count(self,object):
+        review_count = object.product_review.all().count()
+        return review_count 
+        
         
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    
     class Meta:
         model = Product
-        fields = '__all__'       
+        fields = '__all__'   
+        
+      
         
         
         
