dfa644ae20fec35089e4403176be2fda72f2a5eb
rafah <rafah.hawa@yahoo.fr>
2023-10-26 19:37:58 +0200

    show review count and average rate in detail

--- products/serializers.py
+++ products/serializers.py
@@ -37,11 +37,31 @@ class ProductListSerializer(serializers.ModelSerializer):
         
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    review_count = serializers.SerializerMethodField()
+    avg_rate = serializers.SerializerMethodField()
+    avg_rate2 = serializers.SerializerMethodField()
     
     class Meta:
         model = Product
         fields = '__all__'   
-        
+    def get_review_count(self,object):
+        review_count = object.product_review.all().count()
+        return review_count
+    
+    def get_avg_rate (self,object):
+        total = 0
+        reviews = object.product_review.all()
+        for r in reviews :
+            total+=r.rate
+            
+        if len(reviews) > 0:
+            return round(total/len(reviews),2)
+        else:
+            return 0
+    
+    def get_avg_rate2(self,object):
+        avg=object.product_review.aggregate(rate_avg=Avg('rate'))
+        return round(avg['rate_avg'],2) if avg['rate_avg'] else 0
       
         
         
