6ffcd60b47759d537b54518f878e9ea8795cd8ca
rafah <rafah.hawa@yahoo.fr>
2023-10-26 17:39:54 +0200

    show foreign field as str in API

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Product , Brand , Review , ProductImages
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
     class Meta:
         model = Product
         fields = '__all__'
