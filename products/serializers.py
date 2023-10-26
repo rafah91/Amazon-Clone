d5c52045440f9d54145dc7a94546c8c8988eb259
rafah <rafah.hawa@yahoo.fr>
2023-10-26 19:31:38 +0200

    add average pro mode

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product , Brand , Review , ProductImages
+from django.db.models.aggregates import Avg
 
 
     
@@ -9,6 +10,7 @@ class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     review_count = serializers.SerializerMethodField()
     avg_rate = serializers.SerializerMethodField()
+    avg_rate2 = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
@@ -27,6 +29,10 @@ class ProductListSerializer(serializers.ModelSerializer):
             return round(total/len(reviews),2)
         else:
             return 0
+    
+    def get_avg_rate2(self,object):
+        avg=object.product_review.aggregate(rate_avg=Avg('rate'))
+        return round(avg['rate_avg'],2) if avg['rate_avg'] else 0
         
         
 class ProductDetailSerializer(serializers.ModelSerializer):
