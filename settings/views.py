6a39a19e6af074d01364a34a9224600813964191
rafah <rafah.hawa@yahoo.fr>
2023-10-29 20:08:49 +0200

    add home views

--- settings/views.py
+++ settings/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from products.models import Brand, Product, Review
 def home(request):
+    brands = Brand.objects.all()[:10]
+    sale_products = (Product.objects.filter(flag='Sale'))[:10]
+    feature_products = (Product.objects.filter(flag='Feature'))[:6]
+    new_products = (Product.objects.filter(flag='New'))[:10]
+    reviews=Review.objects.all()[:6]
     
-    
-    return render(request,'settings/home.html',{})
+    return render(request,'settings/home.html',{
+        'brands': brands,
+        'sale_products':sale_products,
+        'feature_products':feature_products,
+        'new_products':new_products,
+        'reviews':reviews
+    })
 # Create your views here.
