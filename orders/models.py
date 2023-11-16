dd175970f120cae8fbccdb1ab205564b37461bc6
rafah <rafah.hawa@yahoo.fr>
2023-11-16 21:37:45 +0200

    design order_detail part 1

--- orders/models.py
+++ orders/models.py
@@ -1,13 +1,40 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import datetime
+from products.models import Product
 
+ORDER_STATUS = (
+    ('Recieved','Recieved'),
+    ('Processed','Processed'),
+    ('Shipped','Shipped'),
+    ('Delivered','Delivered'),
+)
 # Create your models here.
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name='order_user',on_delete=models.SET_NULL,null=True, blank=True)
+    code = models.CharField(max_length=10)
+    status = models.CharField(max_length=15, choices=ORDER_STATUS, default='Recieved')
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True, blank=True)
+    delivery_location = ''
+    coupon = models.ForeignKey('Coupon', related_name='order_coupon',on_delete=models.SET_NULL,null=True, blank=True)
+    order_total_discount=models.FloatField(null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.user)
+    
+    
+    
 
 class OrderDetail(models.Model):
-    pass
+    order = models.ForeignKey(Order, related_name='order_detail',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='orderdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = ''
+    brand = ''
+    price = ''
+    total = ''
+    
 
 class Coupon(models.Model):
     code = models.CharField(max_length=15)
