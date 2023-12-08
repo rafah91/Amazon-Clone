d7af98d6c5b4a50c938438319641f3a0755ea5e8
rafah <rafah.hawa@yahoo.fr>
2023-12-08 15:44:17 +0200

    add users models

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import DeliveryAddress
+from .models import DeliveryAddress , Profile , Phones
 
+
+
+
+admin.site.register(Profile)
+admin.site.register(Phones)
 admin.site.register(DeliveryAddress)
