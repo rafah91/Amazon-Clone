b6b2800bed1ec58dadea3c9c228dd55c8ff49c03
rafah <rafah.hawa@yahoo.fr>
2023-12-21 22:47:20 +0200

    add gmail settings to send email

--- project/settings.py
+++ project/settings.py
@@ -198,4 +198,10 @@ LANGUAGES = [
 ]
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' #service that we buy
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "sweetsyrialeena@gmail.com"
+EMAIL_HOST_PASSWORD = "vlllvhvfbmyciezk"
