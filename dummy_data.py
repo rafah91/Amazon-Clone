7cd5ce2922299f0550430b92c5e21c2ca3bb4018
rafah <rafah.hawa@yahoo.fr>
2024-02-14 20:33:27 +0100

    Update dummy data

--- dummy_data.py
+++ dummy_data.py
@@ -1,10 +1,15 @@
-import os , django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+import os , django 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
 django.setup()
+
+
+
 from faker import Faker
 import random
 from products.models import Product , Brand , Review
 from django.contrib.auth.models import User
+
 
 def add_users(n):
     for x in range(n):
@@ -14,53 +19,64 @@ def add_users(n):
             email = fake.email(),
             password = '12345'
         )
- 
 
 
-def add_Brands(n):
+def add_brands(n):
     fake = Faker()
     images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
+    
     for x in range(n):
         Brand.objects.create(
-            name = fake.name() ,
+            name = fake.name() , 
             image = f"brands/{images[random.randint(0,9)]}"
         )
-    print(f'{n} brand was created successfully')
-
+    
+    print(f'{n} Brands was created successfully')
+    
+    
 def add_products(n):
     fake = Faker()
     images = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
     flags = ['Sale','New','Feature']
+    brands = Brand.objects.all()
+    
     for x in range(n):
         Product.objects.create(
-            name = fake.name() ,
+            name = fake.name() , 
             name_ar = f'product-{x}',
             name_en = fake.name() , 
-            subtitle = fake.text(max_nb_chars=300) ,
-            description = fake.text(max_nb_chars=4000) ,
-            image = f"products/{images[random.randint(0,9)]}" ,
-            price = round(random.uniform(20.99,99.99),2) ,
+            subtitle = fake.text(max_nb_chars = 300) , 
+            description = fake.text(max_nb_chars = 4000) , 
+            image = f"products/{images[random.randint(0,9)]}",
+            price = round(random.uniform(20.99,99.99),2) , 
             flag = flags[random.randint(0,2)] , 
-            brand = Brand.objects.get(id=random.randint(1,300)) ,
-            sku = random.randint(1000,1000000) ,
-            quantity = random.randint(1,100) ,
-        )
-    print(f'{n}product was created successfully')
+            brand = random.choice(brands),
+            sku = random.randint(1000,1000000) , 
+            quantity = random.randint(5,100) , 
+        ) 
+
+    print(f'{n} Products was created successfully')
+
+
+
 
 def add_reviews(n):
     fake = Faker()
+    users = User.objects.all()
+    products = Product.objects.all()
+    
     for x in range(n):
         Review.objects.create(
-            user = User.objects.get(id=random.randint(1,5)) ,
-            product = Product.objects.get(id=random.randint(1,500)) ,
-            rate = random.randint(1,5) ,
-            feedback = fake.text(max_nb_chars=200) ,
+            user = random.choice(users),
+            product =  random.choice(products), 
+            rate = random.randint(1,5) , 
+            feedback = fake.text(max_nb_chars=200)
         )
+        
     print(f'{n} Reviews was created successfully')
-    
 
 
-#add_users(20)
-#add_Brands(300)
-add_products(10)
+add_users(10)
+add_brands(20)
+add_products(100)
 add_reviews(100)
--- project/settings.py
+++ project/settings.py
@@ -132,16 +132,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'),conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get('DATABASE_URL'),conn_max_age=600)
+#     }
 
 
 # DATABASES = {
