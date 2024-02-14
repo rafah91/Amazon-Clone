4b0f61e720f89df99ccb0ea13a402077bc39ebbb
rafah <rafah.hawa@yahoo.fr>
2024-02-14 20:04:21 +0100

    Use enviroment variables

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q@72grdmgk^5xqmlmg_i&e=u^&d8t=d0^0gnfl5cb@ka8vfwa0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,7 +140,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse("postgres://mypostgres_ut3a_user:dATXDFC70kJW4lGvpBf0qXDT8Ze2Lbul@dpg-cn4fchvqd2ns73em03d0-a.oregon-postgres.render.com/mypostgres_ut3a",conn_max_age=600)
+    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'),conn_max_age=600)
     }
 
 
